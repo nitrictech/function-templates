2a5c6cb0087e63f7cb5ab3d9de347062a50e076e
Tim Holm <tim.holm@nitric.io>
2021-06-19 15:00:53 +1000

    fix: Add entrypoint to java template

--- templates/java11/template/src/main/java/com/example/Handler.java
+++ templates/java11/template/src/main/java/com/example/Handler.java
@@ -5,11 +5,15 @@ import io.nitric.faas.Trigger;
 import io.nitric.faas.NitricFunction;
 import io.nitric.faas.Response;
 
-public class HelloWorld implements NitricFunction {
+public class Handler implements NitricFunction {
 
     @Override
     public Response handle(Trigger trigger) {
         return trigger.buildResponse("Hello World");
+    }
+
+    public static void main(String[] args) {
+        Faas.start(new Handler());
     }
 
 }
