a778c8582cb790a6f195cb5844e26ce4ed4e2be0
Ryan Cartwright <ryan.cartwright@nitric.io>
2021-06-22 11:35:01 +1000

    feat: Add dotnet template

---
+++ templates/dotnet/function/src/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Nitric.Faas;
+
+namespace NitricTemplate
+{
+    class HelloWorld : INitricFunction
+    {
+        public Response Handle(Trigger trigger){
+            return trigger.DefaultResponse(
+                Encoding.UTF8.GetBytes("Hello World!")
+            );
+        }
+        static void Main(string[] args)
+        {
+            Faas.Start(new HelloWorld());
+        }
+    }
+}
